5a2860e7ddd73d3b660d6efb44e4245620abfad8
callummubery <cmubo@hotmail.com>
2025-05-14 16:30:39 +0100

    Added split costs and view is now scrollable

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,57 +1,79 @@
-import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import { Pressable } from "react-native";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
 
-import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
-import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
     name: React.ComponentProps<typeof FontAwesome>["name"];
     color: string;
 }) {
-    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+    return <FontAwesome size={20} {...props} />;
+}
+
+interface TabIconProps {
+    focused: boolean;
+    icon: React.ReactElement;
+    title: string;
+}
+
+function TabIcon({ focused, icon, title }: TabIconProps) {
+    return (
+        <View
+            className={`flex flex-row w-full  min-w-[112px] min-h-[52px] justify-center items-center rounded-full overflow-hidden mt-4 ${focused ? "bg-blue-500" : ""}`}
+        >
+            {icon}
+            <Text className="text-white text-base font-semibold ml-2">
+                {title}
+            </Text>
+        </View>
+    );
 }
 
 export default function TabLayout() {
     const colorScheme = useColorScheme();
+    const insets = useSafeAreaInsets();
 
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-                // Disable the static render of the header on web
-                // to prevent a hydration error in React Navigation v6.
-                headerShown: useClientOnlyValue(false, true),
+                headerShown: false,
+                tabBarItemStyle: {
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                tabBarStyle: {
+                    backgroundColor: "#0f0d23",
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom: insets.bottom,
+                    paddingBottom: 0,
+                    height: 52,
+                    position: "absolute",
+                    overflow: "hidden",
+                    borderWidth: 1,
+                    borderColor: "#0f0d23",
+                    bottom: 0,
+                },
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: "white",
             }}
         >
             <Tabs.Screen
                 name="index"
                 options={{
                     title: "Tab One",
-                    tabBarIcon: ({ color }) => (
-                        <TabBarIcon name="code" color={color} />
-                    ),
-                    headerRight: () => (
-                        <Link href="/modal" asChild>
-                            <Pressable>
-                                {({ pressed }) => (
-                                    <FontAwesome
-                                        name="info-circle"
-                                        size={25}
-                                        color={
-                                            Colors[colorScheme ?? "light"].text
-                                        }
-                                        style={{
-                                            marginRight: 15,
-                                            opacity: pressed ? 0.5 : 1,
-                                        }}
-                                    />
-                                )}
-                            </Pressable>
-                        </Link>
+                    tabBarIcon: ({ focused, color }) => (
+                        <TabIcon
+                            focused={focused}
+                            title="Home"
+                            icon={<TabBarIcon name="code" color={color} />}
+                        />
                     ),
                 }}
             />
@@ -59,8 +81,12 @@ export default function TabLayout() {
                 name="two"
                 options={{
                     title: "Tab Two",
-                    tabBarIcon: ({ color }) => (
-                        <TabBarIcon name="code" color={color} />
+                    tabBarIcon: ({ focused, color }) => (
+                        <TabIcon
+                            focused={focused}
+                            title="Tab 2"
+                            icon={<TabBarIcon name="code" color={color} />}
+                        />
                     ),
                 }}
             />
