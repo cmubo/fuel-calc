d9b06e5b41886c3c18483c69f0cc70cda3e38788
callummubery <cmubo@hotmail.com>
2025-05-20 16:07:50 +0100

    Installed react query and used it on the calculator screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,57 @@
-import { fuelPricesTable } from "@/db/schema";
 import { getDefaultFuelPrice } from "@/features/fuel-prices/db";
 import JourneyForm from "@/features/journeys/components/JourneyForm";
+import { useQuery } from "@tanstack/react-query";
 import { useSQLiteContext } from "expo-sqlite";
-import { Suspense, use } from "react";
 import {
     ActivityIndicator,
     SafeAreaView,
     ScrollView,
+    Text,
     View,
 } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function CalculatorScreenWrapper() {
-    const sqliteContext = useSQLiteContext();
-    const defaultFuelPricePromise = getDefaultFuelPrice(sqliteContext);
-
     return (
         <SafeAreaView className="bg-slate-950">
-            <Suspense
-                fallback={
-                    <View className="flex items-center justify-center w-full h-full flex-col">
-                        <ActivityIndicator
-                            size="large"
-                            color="#0000ff"
-                            className="my-3"
-                        />
-                    </View>
-                }
-            >
-                <CalculatorScreen
-                    defaultFuelPricePromise={defaultFuelPricePromise}
-                />
-            </Suspense>
+            <CalculatorScreen />
         </SafeAreaView>
     );
 }
 
-interface CalculatorScreenProps {
-    defaultFuelPricePromise: Promise<(typeof fuelPricesTable.$inferSelect)[]>;
-}
+function CalculatorScreen() {
+    const sqliteContext = useSQLiteContext();
 
-function CalculatorScreen({ defaultFuelPricePromise }: CalculatorScreenProps) {
     const insets = useSafeAreaInsets();
-    const defaultFuelPrice = use(defaultFuelPricePromise);
+
+    const {
+        data: defaultFuelPrice,
+        isPending,
+        isError,
+    } = useQuery({
+        queryKey: ["defaultFuelPrice"],
+        queryFn: getDefaultFuelPrice.bind(null, sqliteContext),
+    });
+
+    if (isPending) {
+        return (
+            <View className="flex items-center justify-center w-full h-full flex-col">
+                <ActivityIndicator
+                    size="large"
+                    color="#0000ff"
+                    className="my-3"
+                />
+            </View>
+        );
+    }
+
+    if (isError) {
+        return (
+            <View className="flex items-center justify-center w-full h-full flex-col">
+                <Text>There was an error</Text>
+            </View>
+        );
+    }
 
     return (
         <ScrollView
