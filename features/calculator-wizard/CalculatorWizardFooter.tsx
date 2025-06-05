c22033f3ec3b594179af9e1bef9c3e1e83d8de66
callummubery <cmubo@hotmail.com>
2025-06-05 14:42:22 +0100

    Footer animations done

--- features/calculator-wizard/CalculatorWizardFooter.tsx
+++ features/calculator-wizard/CalculatorWizardFooter.tsx
@@ -1,12 +1,36 @@
 import HeroIcon from "@/components/icons/HeroIcon";
+import { GroteskText } from "@/components/StyledText";
 import { twColors } from "@/constants/Colors";
 import { JourneyRawFormValues } from "@/features/journeys/hooks/useJourneyForm";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 import { UseFormReturn } from "react-hook-form";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Animated, {
+    FadeIn,
+    FadeInLeft,
+    FadeOut,
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming,
+} from "react-native-reanimated";
 import { STEPS } from "./constants";
 import { PreviousFormValueType } from "./types";
 
+const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
+const AnimatedHeroIcon = Animated.createAnimatedComponent(HeroIcon);
+
+const NEXT_BUTTON_WIDTH_SMALL = 126;
+const NEXT_BUTTON_WIDTH_LARGE = 160;
+const NEXT_BUTTON_ARROW_SIZE = 30;
+const NEXT_BUTTON_HORIZONTAL_PADDING = 16;
+
+function calulateArrowXPosition(buttonSize: "large" | "small") {
+    const buttonWidth =
+        buttonSize === "large"
+            ? NEXT_BUTTON_WIDTH_LARGE
+            : NEXT_BUTTON_WIDTH_SMALL;
+    return -(buttonWidth - NEXT_BUTTON_ARROW_SIZE) / 2;
+}
 export default function CalculatorWizardFooter({
     handleNavigation,
     index,
@@ -18,9 +42,38 @@ export default function CalculatorWizardFooter({
     form: UseFormReturn<JourneyRawFormValues, any, JourneyRawFormValues>;
     setPreviousValues: Dispatch<SetStateAction<PreviousFormValueType[]>>;
 }) {
-    const onNavigation = () => {
-        const currentStep = STEPS[index.toString()];
+    const currentStep = STEPS[index.toString()];
+    const nextButtonWidth = useSharedValue(126);
+    const nextButtonArrowTranslateX = useSharedValue(
+        -NEXT_BUTTON_HORIZONTAL_PADDING,
+    );
 
+    const nextButtonAnimatedStyles = useAnimatedStyle(() => ({
+        width: withTiming(nextButtonWidth.value, { duration: 300 }),
+    }));
+
+    const nextButtonArrowStyles = useAnimatedStyle(() => ({
+        transform: [
+            {
+                translateX: withTiming(nextButtonArrowTranslateX.value, {
+                    duration: 300,
+                }),
+            },
+        ],
+    }));
+
+    useEffect(() => {
+        if (currentStep.name === "result") {
+            nextButtonWidth.value = NEXT_BUTTON_WIDTH_LARGE;
+            nextButtonArrowTranslateX.value = -16;
+        } else {
+            nextButtonWidth.value = NEXT_BUTTON_WIDTH_SMALL;
+
+            nextButtonArrowTranslateX.value = calulateArrowXPosition("small");
+        }
+    }, [currentStep]);
+
+    const onNavigation = () => {
         if (currentStep.field !== null) {
             const validation = form.trigger(
                 currentStep.field as keyof JourneyRawFormValues,
@@ -69,9 +122,40 @@ export default function CalculatorWizardFooter({
 
     return (
         <View style={styles.footerContainer}>
-            <TouchableOpacity onPress={onNavigation} style={styles.nextButton}>
-                <HeroIcon icon="arrow-long-right" color="white" size={30} />
-            </TouchableOpacity>
+            <View>
+                {currentStep.name === "result" ? (
+                    <Animated.View entering={FadeIn} exiting={FadeOut}>
+                        <GroteskText>Reset?</GroteskText>
+                    </Animated.View>
+                ) : null}
+            </View>
+
+            <AnimatedTouchable
+                onPress={onNavigation}
+                style={[styles.nextButton, nextButtonAnimatedStyles]}
+            >
+                <View>
+                    {currentStep.name === "result" ? (
+                        <Animated.View
+                            entering={FadeInLeft}
+                            exiting={FadeOut.duration(100)}
+                        >
+                            <GroteskText style={{ fontSize: 12 }}>
+                                Save Journey?
+                            </GroteskText>
+                        </Animated.View>
+                    ) : null}
+                </View>
+                <AnimatedHeroIcon
+                    icon="arrow-long-right"
+                    color="white"
+                    size={NEXT_BUTTON_ARROW_SIZE}
+                    style={[
+                        { position: "absolute", right: 0 },
+                        nextButtonArrowStyles,
+                    ]}
+                />
+            </AnimatedTouchable>
         </View>
     );
 }
@@ -90,19 +174,22 @@ const styles = StyleSheet.create({
         padding: 24,
         width: "100%",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
     },
     nextButton: {
+        position: "relative",
+        height: 54,
         backgroundColor: twColors.sky[950],
         paddingVertical: 12,
-        paddingHorizontal: 48,
+        paddingHorizontal: NEXT_BUTTON_HORIZONTAL_PADDING,
         borderRadius: 8,
-        justifyContent: "center",
         alignItems: "center",
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
             height: 2,
         },
+        flexDirection: "row",
     },
 });
