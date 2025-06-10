95163dd7d863cd711df876ebb21d3430fe4acdfe
callummubery <cmubo@hotmail.com>
2025-06-10 19:15:37 +0100

    Added date of journey to edit journey form

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useController } from "react-hook-form";
 import RNDatePicker, {
     DatePickerProps as RNDatePickerProps,
 } from "react-native-date-picker";
 
-interface DatePickerProps extends Omit<RNDatePickerProps, "date"> {
+interface DatePickerProps
+    extends Omit<RNDatePickerProps, "date" | "open" | "setOpen"> {
     name: string;
+    open?: boolean;
+    setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function DatePicker({ name, ...props }: DatePickerProps) {
+export default function DatePicker({
+    name,
+    open,
+    setOpen,
+    ...props
+}: DatePickerProps) {
     const { field } = useController({ name });
     const [date, setDate] = useState(new Date(field.value));
+
+    const onSelectCallback = useCallback((value: Date) => {
+        setDate(value);
+        field.onChange(value.toISOString().split("T")[0]);
+
+        setOpen ? setOpen(false) : null;
+    }, []);
 
     return (
         <>
             <RNDatePicker
                 date={date}
                 mode="date"
-                onDateChange={(date) => {
-                    setDate(date);
-
-                    field.onChange(date.toISOString().split("T")[0]);
-                }}
+                onDateChange={onSelectCallback}
+                onConfirm={onSelectCallback}
+                onCancel={() => (setOpen ? setOpen(false) : null)}
+                open={open}
                 {...props}
             />
         </>
